a32df68cc476bc46ea2b7a2b46cd2fb6b1374809
Novasis <novasis@new-host-3.station>
2016-10-30 09:26:58 +0100

    Modified .gitignoer

--- NOVAsomP/include/generated/timestamp_autogenerated.h
+++ NOVAsomP/include/generated/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
-#define U_BOOT_DATE "Oct 27 2016"
-#define U_BOOT_TIME "16:14:05"
+#define U_BOOT_DATE "Oct 29 2016"
+#define U_BOOT_TIME "10:14:05"
--- NOVAsomP/include/generated/version_autogenerated.h
+++ NOVAsomP/include/generated/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2015.04-gc449b2a-dirty"
+#define PLAIN_VERSION "2015.04-g1a5846f-dirty"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CC_VERSION_STRING "arm-linux-gcc.br_real (Buildroot 2016.05) 5.4.0"
 #define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.25.1"
