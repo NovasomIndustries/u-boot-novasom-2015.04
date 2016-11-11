118f9229634582018407f0504251805816f127e3
NovasomIndustries <filippo.visocchi@novasomindustries.com>
2016-11-11 14:06:49 +0100

    "Some fixes"

--- NOVAsomS/include/generated/timestamp_autogenerated.h
+++ NOVAsomS/include/generated/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Nov 11 2016"
-#define U_BOOT_TIME "13:12:48"
+#define U_BOOT_TIME "14:00:14"
--- NOVAsomS/include/generated/version_autogenerated.h
+++ NOVAsomS/include/generated/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2015.04-gdbff929-dirty"
+#define PLAIN_VERSION "2015.04-g29c153d-dirty"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CC_VERSION_STRING "arm-linux-gcc.br_real (Buildroot 2016.05) 5.4.0"
 #define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.25.1"
